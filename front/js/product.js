d53947076c7e2bf1179612cf55b59dfb243ce7e5
nitaka <grossoolive@yahoo.fr>
2021-11-23 19:22:42 +0100

    finition product.js

--- front/js/product.js
+++ front/js/product.js
@@ -56,6 +56,72 @@ function getProduct(article){
         productColors.value = colors;
         productColors.innerHTML = colors;
     }
+
     addToCart(article);
 }
 
+//Gestion du panier
+function addToCart(article) {
+    const btnPostPanier = document.querySelector("#addToCart");
+
+    //Ecoute et initialisation couleur et quantité
+    btnPostPanier.addEventListener("click", (e) => {
+        if (quantitySofa.value > 0 && quantitySofa.value <=100 && quantitySofa.value != 0) {
+
+        //Recupération du choix de la couleur
+        let choiceColor = colorSofa.value;
+                    
+        //Recupération du choix de la quantité
+        let choiceAmont = quantitySofa.value;
+
+        //Récupération objet options de l'article à ajouter au panier
+        let optionsProduit = {
+            idProduit: idProduct,
+            couleurProduit: choiceColor,
+            quantiteProduit: Number(choiceAmont),
+            nomProduit: article.name,
+            prixProduit: article.price,
+            descriptionProduit: article.description,
+            imgProduit: article.imageUrl,
+            altImgProduit: article.altTxt
+        };
+
+        //Initialisation du local storage
+        let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+
+        //Ajout panier
+        const addPanier = () => {
+            window.location.href ="cart.html";
+        }
+
+    //Importation dans le local storage
+    //Si le panier comporte déjà au moins 1 article
+    if (produitLocalStorage) {
+    const resultFind = produitLocalStorage.find(
+        (el) => el.idProduit === idProduct && el.couleurProduit === choiceColor);
+        
+        //Si le produit commandé est déjà dans le panier
+        if (resultFind) {
+            let newQuantite =
+            parseInt(optionsProduit.quantiteProduit) + parseInt(resultFind.quantiteProduit);
+            resultFind.quantiteProduit = newQuantite;
+            localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+            console.table(produitLocalStorage);
+            addPanier();
+        //Si le produit commandé n'est pas dans le panier
+        } else {
+            produitLocalStorage.push(optionsProduit);
+            localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+            console.table(produitLocalStorage);
+            addPanier();
+        }
+    //Si le panier est vide
+    } else {
+        produitLocalStorage =[];
+        produitLocalStorage.push(optionsProduit);
+        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+        console.table(produitLocalStorage);
+        addPanier();
+    }}
+    });
+}
